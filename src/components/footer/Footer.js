0f1f903ea5708aa20a0d97b822f9986fd5b955d6
Wilfred <700024165@student.curtin.edu.my>
2024-05-12 08:14:48 +0800

    res fix, emailStatus prompt

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,12 +7,12 @@ export default function Footer(){
     return(
         <footer className="footer">
             <ul className = "power-logo">
-                <a href="https://html.com/" style={{ color: 'whitesmoke'}}><FaHtml5 /></a>
-                <a href="https://en.wikipedia.org/wiki/CSS" style={{ color: 'whitesmoke'}}><FaCss3 /></a>
-                <a href="https://www.javascript.com/" style={{ color: 'whitesmoke'}}><IoLogoJavascript /></a>
-                <a href="https://react.dev/" style={{ color: 'whitesmoke'}}><FaReact /></a>
-                <a href="https://nodejs.org/en" style={{ color: 'whitesmoke'}}><FaNodeJs /></a>
-                <a href="https://stackoverflow.com/" style={{ color: 'whitesmoke'}}><FaStackOverflow /></a>
+                <a href="https://html.com/" style={{ color: 'whitesmoke'}} target="_blank" rel="noreferrer noopener"><FaHtml5 /></a>
+                <a href="https://en.wikipedia.org/wiki/CSS" style={{ color: 'whitesmoke'}} target="_blank" rel="noreferrer noopener"><FaCss3 /></a>
+                <a href="https://www.javascript.com/" style={{ color: 'whitesmoke'}} target="_blank" rel="noreferrer noopener"><IoLogoJavascript /></a>
+                <a href="https://react.dev/" style={{ color: 'whitesmoke'}} target="_blank" rel="noreferrer noopener"><FaReact /></a>
+                <a href="https://nodejs.org/en" style={{ color: 'whitesmoke'}} target="_blank" rel="noreferrer noopener"><FaNodeJs /></a>
+                <a href="https://stackoverflow.com/" style={{ color: 'whitesmoke'}} target="_blank" rel="noreferrer noopener"><FaStackOverflow /></a>
             </ul>
         </footer>
     )
