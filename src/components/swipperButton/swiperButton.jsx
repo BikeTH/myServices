e6dfa62e46a9e255a9f830527dda0aee193240b7
Wilfred <700024165@student.curtin.edu.my>
2024-05-13 20:43:07 +0800

    nextButton improvement

--- src/components/swipperButton/swiperButton.jsx
+++ src/components/swipperButton/swiperButton.jsx
@@ -2,12 +2,22 @@ import React from 'react';
 import { useSwiper } from 'swiper/react';
 import "./swipperButton.css"
 
+const scrollToTop = () => {
+  document.body.scrollTop = 500;
+  document.documentElement.scrollTop = 500;
+};
+
 export function SwiperNavNext(){
   const swiper = useSwiper();
 
+  const handleNextClick = () =>{
+    swiper.slideNext();
+    scrollToTop();
+  };
+
   return (
     <div className="next-btns">
-      <button className="next-Assign" onClick={() => swiper.slideNext()}>Assignment Aid &gt;&gt;</button>
+      <button className="next-Assign" onClick={handleNextClick}>Assignment Aid &gt;&gt;</button>
     </div>
   );
 };
@@ -15,9 +25,14 @@ export function SwiperNavNext(){
 export function SwiperNavPrev() {
     const swiper = useSwiper();
 
+    const handlePrevClick = () =>{
+      swiper.slidePrev();
+      scrollToTop();
+    };
+
     return(
         <div className="prev-btns">
-        <button className="prev-Assign" onClick={() => swiper.slidePrev()}>&lt;&lt; Unit Trust</button>
+        <button className="prev-Assign" onClick={handlePrevClick}>&lt;&lt; Unit Trust</button>
       </div>
     )
 }
